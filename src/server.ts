f8faa1f3d0fa789b6da6ca0ddc7bb82be474f302
ruhancmendes <ruhancmendes@gmail.com>
2025-08-13 16:46:43 -0300

    git init + configurando o projeto

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+// Servidor 
+
+import express from 'express';
+
+const PORT = 3333
+const app = express()
+app.use(express.json())
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
