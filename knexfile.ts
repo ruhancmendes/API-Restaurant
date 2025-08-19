025ee5a9602f1d8a7207a30e103c53f1fb9bcea8
ruhancmendes <ruhancmendes@gmail.com>
2025-08-19 18:09:24 -0300

    install e config knex - habilitando restrição de relacionamentos

---
+++ knexfile.ts
@@ -0,0 +1,24 @@
+// Configurando o knex para usar SQLite3
+
+export default {
+    client: "sqlite3",
+    connection: {
+        filename: "./src/database/database.db",
+    },
+    //Habilitando a restrição de chaves estrangeiras
+    pool: {
+        afterCreate: (connection: any, done: any) => {
+            connection.run("PRAGMA foreign_keys = ON")
+            done()
+        },
+    },
+    useNullAsDefault: true,
+    migrations: {
+        extensions: "ts",
+        directory: "./src/database/migrations",
+    },
+    seeds: {
+        extensions: "ts",
+        directory: "./src/database/seeds",
+    },
+}
